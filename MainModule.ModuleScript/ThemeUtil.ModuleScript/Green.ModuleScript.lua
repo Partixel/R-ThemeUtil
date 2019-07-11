dcbf84bd629107f804bc38d6a0dd9e77adc1b691
Nick (Partixel) Lock <partixel@v-handle.com>
2019-07-11 20:57:20 +0100

    Update Green theme

--- MainModule.ModuleScript/ThemeUtil.ModuleScript/Green.ModuleScript.lua
+++ MainModule.ModuleScript/ThemeUtil.ModuleScript/Green.ModuleScript.lua
@@ -6,7 +6,9 @@ return {
 		
 		Primary_BackgroundColor = Color3.fromRGB( 0, 102, 56 ),
 		
-		Secondary_BackgroundColor = Color3.fromRGB( 0, 127, 70 )
+		Secondary_BackgroundColor = Color3.fromRGB( 0, 127, 70 ),
+		
+		Inverted_BackgroundColor = Color3.fromRGB( 93, 133, 115 ),
 		
 	}
 	
