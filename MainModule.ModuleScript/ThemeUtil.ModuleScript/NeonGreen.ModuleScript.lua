2f368d2516e61feaf9ef87b231717e95a07410e2
Nick (Partixel) Lock <partixel@v-handle.com>
2019-07-11 20:45:37 +0100

    Add NeonGreen theme

---
+++ MainModule.ModuleScript/ThemeUtil.ModuleScript/NeonGreen.ModuleScript.lua
@@ -0,0 +1,19 @@
+return {
+	
+	Inherits = "Dark",
+	
+	Theme = {
+		
+		Primary_BackgroundColor = Color3.fromRGB( 0,170,127 ),
+		
+		Primary_BackgroundTransparency = 0.25,
+		
+		Secondary_BackgroundTransparency = 0.3,
+		
+		Secondary_BackgroundColor = Color3.fromRGB( 0,200,140 ),
+		
+		Secondary_TextColor = Color3.fromRGB( 220, 220, 220 )
+		
+	}
+	
+}
