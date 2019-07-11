8dda239c2e856f785f62ad547903f45b3e0e6cd6
Nick (Partixel) Lock <partixel@v-handle.com>
2019-07-11 20:47:12 +0100

    Optimise theming of theme gui

--- MainModule.ModuleScript/ThemeGui.ScreenGui/ThemeMain.LocalScript.lua
+++ MainModule.ModuleScript/ThemeGui.ScreenGui/ThemeMain.LocalScript.lua
@@ -181,45 +181,29 @@ end
 
 if script.Parent:FindFirstChild( "Toggle" ) then
 	
-	function UpdateColor( )
-		
-		script.Parent.Toggle.BackgroundColor3 = script.Parent.Open.Value and ThemeUtil.GetThemeFor( "Positive_Color3" ) or ThemeUtil.GetThemeFor( "Primary_BackgroundColor" )
-		
-		script.Parent.AltToggle.BackgroundColor3 = script.Parent.Toggle.BackgroundColor3
+	local function HandleTransparency( Obj )
 		
 		local Transparency = ThemeUtil.GetThemeFor( "Primary_BackgroundTransparency" )
 		
-		script.Parent.Toggle.BackgroundTransparency = Transparency
-		
-		script.Parent.AltToggle.BackgroundTransparency = Transparency
+		Obj.BackgroundTransparency = Transparency
 		
 		if Transparency > 0.9 then
 			
-			script.Parent.Toggle.TextStrokeColor3 = ThemeUtil.GetThemeFor( "Primary_TextColor" )
+			ThemeUtil.BindUpdate( Obj, { TextColor3 = script.Parent.Open.Value and "Selection_Color3" or "Primary_BackgroundColor" } )
 			
-			script.Parent.Toggle.TextColor3 = script.Parent.Open.Value and ThemeUtil.GetThemeFor( "Positive_Color3" ) or ThemeUtil.GetThemeFor( "Primary_BackgroundColor" )
-			
-			script.Parent.Toggle.TextStrokeTransparency = 0
-			
-			script.Parent.AltToggle.TextStrokeColor3 = script.Parent.Toggle.TextStrokeColor3
-			
-			script.Parent.AltToggle.TextColor3 = script.Parent.Toggle.TextColor3
-			
-			script.Parent.AltToggle.TextStrokeTransparency = 0
+			Obj.TextStrokeTransparency = 0
 			
 		else
 			
-			script.Parent.Toggle.TextColor3 = ThemeUtil.GetThemeFor( "Primary_TextColor" )
+			ThemeUtil.BindUpdate( Obj, { TextColor3 = "Primary_TextColor" } )
 			
-			script.Parent.Toggle.TextStrokeTransparency = 1
-			
-			script.Parent.AltToggle.TextColor3 = script.Parent.Toggle.TextColor3
-			
-			script.Parent.AltToggle.TextStrokeTransparency = 1
+			Obj.TextStrokeTransparency = 1
 			
 		end
 		
 	end
+	
+	ThemeUtil.BindUpdate( { script.Parent.Toggle, script.Parent.AltToggle }, { BackgroundColor3 = script.Parent.Open.Value and "Selection_Color3" or "Primary_BackgroundColor", TextTransparency = "Primary_TextTransparency", TextStrokeColor3 = "Primary_TextColor", BackgroundTransparency = HandleTransparency } )
 	
 	function ToggleGui( )
 		
@@ -237,7 +221,7 @@ if script.Parent:FindFirstChild( "Toggle" ) then
 			
 			TweenService:Create( script.Parent.ThemeFrame, TweenInfo.new( 0.5, Enum.EasingStyle.Sine ), { Position = UDim2.new( 0.15, 0, 0.43, 0 ), Size = UDim2.new( 0.2, 0, 0.4, 0 ) } ):Play( )
 			
-			UpdateColor( )
+			ThemeUtil.BindUpdate( { script.Parent.Toggle, script.Parent.AltToggle }, { BackgroundColor3 = script.Parent.Open.Value and "Selection_Color3" or "Primary_BackgroundColor" } )
 			
 		else
 			
@@ -257,15 +241,11 @@ if script.Parent:FindFirstChild( "Toggle" ) then
 			
 			Tween:Play( )
 			
-			UpdateColor( )
+			ThemeUtil.BindUpdate( { script.Parent.Toggle, script.Parent.AltToggle }, { BackgroundColor3 = script.Parent.Open.Value and "Selection_Color3" or "Primary_BackgroundColor" } )
 			
 		end
 		
 	end
-	
-	ThemeUtil.BindUpdate( script.Parent.Toggle, { BackgroundColor3 = UpdateColor, TextTransparency = "Primary_TextTransparency" } )
-	
-	ThemeUtil.BindUpdate( script.Parent.AltToggle, { TextTransparency = "Primary_TextTransparency" } )
 	
 	script.Parent.ThemeFrame.Position = script.Parent.AltToggle.Position
 	
