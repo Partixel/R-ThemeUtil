8e1896c7e368511225f7b3f28256cb8fd06b4d02
Nick (Partixel) Lock <partixel@v-handle.com>
2019-09-07 11:47:47 +0100

    Removed unused server-side datastore code

--- MainModule.ModuleScript.lua
+++ MainModule.ModuleScript.lua
@@ -10,40 +10,4 @@ LoaderModule( script:WaitForChild( "ReplicatedStorage" ) )
 
 LoaderModule( script:WaitForChild( "MenuModules" ), game:GetService( "ServerStorage" ):WaitForChild( "MenuModules" ) )
 
-local DataStore2 = require( 1936396537 )
-
-DataStore2.Combine( "PartixelsVeryCoolMasterKey", "Theme1" )
-
-local ThemeRemote = Instance.new( "RemoteEvent" )
-
-ThemeRemote.Name = "ThemeRemote"
-
-ThemeRemote.OnServerEvent:Connect( function ( Plr, Theme )
-	
-	DataStore2( "Theme1", Plr ):Set( Theme )
-	
-end )
-
-ThemeRemote.Parent = game:GetService( "ReplicatedStorage" ).ThemeUtil
-
-function HandlePlr( Plr )
-	
-	local Theme = DataStore2( "Theme1", Plr ):Get( )
-	
-	if Theme then
-		
-		ThemeRemote:FireClient( Plr, Theme )
-		
-	end
-	
-end
-
-for _, Plr in ipairs( game:GetService( "Players" ):GetPlayers( ) ) do
-	
-	HandlePlr( Plr )
-	
-end
-
-game.Players.PlayerAdded:Connect( HandlePlr )
-
 return nil
