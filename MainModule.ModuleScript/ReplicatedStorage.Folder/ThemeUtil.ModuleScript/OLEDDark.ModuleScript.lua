08560aaedef3f081a65c1f6bac098fcc9e9b959b
Nick (Partixel) Lock <partixel@v-handle.com>
2019-12-07 17:11:59 +0000

    Use Dark Inverted_TextColor for OLEDDark

--- MainModule.ModuleScript/ReplicatedStorage.Folder/ThemeUtil.ModuleScript/OLEDDark.ModuleScript.lua
+++ MainModule.ModuleScript/ReplicatedStorage.Folder/ThemeUtil.ModuleScript/OLEDDark.ModuleScript.lua
@@ -8,8 +8,6 @@ return {
 		
 		Secondary_BackgroundColor = Color3.fromRGB( 0, 0, 0 ),
 		
-		Inverted_TextColor = Color3.fromRGB( 0, 0, 0 ),
-		
 		Secondary_TextColor = Color3.fromRGB( 200, 200, 200 )
 		
 	}
